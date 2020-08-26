b5d7efa2c54628d1d1958de4e2b6249ff9935d5a
Metin Suloglu <metinsuloglu97@gmail.com>
2020-08-26 10:33:41 +0100

    Update scripts

--- pointnet_model/tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ pointnet_model/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /tensorflow-1.15.2/python2.7/tensorflow_core/include -I /usr/local/cuda-10.1/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-10.1/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- pointnet_model/tf_ops/grouping/tf_grouping_compile.sh
+++ pointnet_model/tf_ops/grouping/tf_grouping_compile.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 /usr/local/cuda-10.1/bin/nvcc tf_grouping_g.cu -o tf_grouping_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /tensorflow-1.15.2/python2.7/tensorflow_core/include -I /usr/local/cuda-10.1/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-10.1/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-10.1/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
